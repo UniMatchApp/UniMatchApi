92ecab9fa52f4a6b2e6188b770a8cd440ecc01c2
Imanol Benito Quintero Bermúdez <imanolquinber26@gmail.com>
2024-10-12 18:48:28 +0100

    feat: add DeleteAllNotificationsCommand logic.

---
+++ core/uniMatch/notifications/domain/application/commands/DeleteAllNotificationsCommand.ts
@@ -0,0 +1,40 @@
+import { ICommand } from "../../../../shared/application/ICommand";
+import { Result } from "../../../../shared/domain/Result";
+import { Event } from "../../domain/Event";
+import { DeleteEventDTO } from "../DTO/DeleteEventDTO";
+import { IEventRepository } from "../ports/IEventRepository";
+import { IEventBus } from "../../../../shared/application/IEventBus";
+import { EventIsDeleted } from "../../domain/events/EventIsDeletedEvent";
+import { UUID } from "../../../../shared/domain/UUID";
+
+
+export class DeleteEventCommand implements ICommand<DeleteEventDTO, void> {
+    private repository: IEventRepository;
+    private eventBus: IEventBus;
+
+    run(request: DeleteEventDTO): Result<void> {
+         
+       try {
+        
+            // busco evento en repositorio
+            const eventId = UUID.fromString(request.eventId); // Transformar el string a UUID
+            const event = this.repository.findById(eventId);
+
+            if (!event) {
+                return Result.failure<void>("Event not found");
+            }
+
+            this.repository.deleteById(eventId);
+
+            // Publicar el evento de dominio EventIsDeleted
+            const eventDeleted = EventIsDeleted.from(event);
+            
+            this.eventBus.publish([eventDeleted]);
+
+            return Result.success<void>(undefined);  // TODO
+            
+        } catch (error) {
+            return Result.failure<void>(error);
+        }
+    }
+}
