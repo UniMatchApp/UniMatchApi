75cb26ce04004cc8cf0df335c8f45a9faaf6ae3b
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-10-16 20:10:34 +0100

    modified events for event service

--- core/uniMatch/event/domain/events/EventIsModifiedEvent.ts
+++ core/uniMatch/event/domain/events/EventIsModifiedEvent.ts
@@ -6,18 +6,21 @@ export class EventIsModified extends DomainEvent {
         aggregateId: string,
         title: string,
         location: string,
+        participants: string[],
         thumbnail?: string
     ) {
         super(aggregateId, "event-is-modified");
         this.getPayload().set("title", title);
         this.getPayload().set("location", location);
+        this.getPayload().set("participants", participants.join(","));
     }
 
     public static from(event: Event): EventIsModified {
         return new EventIsModified(
             event.getId().toString(),
             event.title,
-            event.location.toString()
+            event.location.toString(),
+            event.participants
         );
     }
 }
