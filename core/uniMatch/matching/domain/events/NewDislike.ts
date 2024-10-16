cf66ade28f4fddf55e94fad23d2a08499deedf7e
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-10-16 20:09:53 +0100

    modified events for matching service

--- core/uniMatch/matching/domain/events/NewDislike.ts
+++ core/uniMatch/matching/domain/events/NewDislike.ts
@@ -1,16 +1,18 @@
 import { DomainEvent } from "@/core/shared/domain/DomainEvent";
 import { Profile } from "../Profile";
 
-export class NewDislike extends DomainEvent {
-    constructor(aggregateId: string, dislikedProfileId: string) {
-        super(aggregateId, "new-dislike");
-        this.getPayload().set("dislikedProfileId", dislikedProfileId);
+export class NewLike extends DomainEvent {
+    constructor(aggregateId: string, user: string, target: string) {
+        super(aggregateId, "new-like");
+        this.getPayload().set("user", user);
+        this.getPayload().set("target", target);
     }
 
-    public static from(senderProfile: Profile, dislikedProfileId: string): NewDislike {
-        return new NewDislike(
-            senderProfile.getId().toString(),
-            dislikedProfileId
+    public static from(aggregateId: string, user: Profile, target: Profile): NewLike {
+        return new NewLike(
+            aggregateId,
+            user.getId().toString(),
+            target.getId().toString()
         );
     }
 }
--- core/uniMatch/matching/domain/events/NewLike.ts
+++ core/uniMatch/matching/domain/events/NewLike.ts
@@ -1,16 +1,18 @@
 import { DomainEvent } from "@/core/shared/domain/DomainEvent";
 import { Profile } from "../Profile";
 
-export class NewLike extends DomainEvent {
-    constructor(aggregateId: string, likedProfileId: string) {
-        super(aggregateId, "new-like");
-        this.getPayload().set("likedProfileId", likedProfileId);
+export class NewDislike extends DomainEvent {
+    constructor(aggregateId: string, user: string, target: string) {
+        super(aggregateId, "new-dislike");
+        this.getPayload().set("user", user);
+        this.getPayload().set("target", target);
     }
 
-    public static from(senderProfile: Profile, likedProfileId: string): NewLike {
-        return new NewLike(
-            senderProfile.getId().toString(),
-            likedProfileId
+    public static from(aggregateId: string, user: Profile, target: Profile): NewDislike {
+        return new NewDislike(
+            aggregateId,
+            user.getId().toString(),
+            target.getId().toString()
         );
     }
 }
